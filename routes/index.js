1ce0a372de966b6b57e0676da7afd72b0d0df790
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-03-06 18:51:47 -0800

    delete function works

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
+//Import dependency for express
 const path = require('path');
 const router = require('express').Router();
 const api = require('./api');
 
 router.use('/api', api);
 
+//define pathway for /notes page
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
 
+//define pathway for all others - ie the homepage
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
